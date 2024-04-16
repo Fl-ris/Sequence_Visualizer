89969f84275c6676d7cec9d0104f651b4ede0efd
Fl-ris <florismenninga@gmail.com>
2024-04-16 18:10:23 +0200

    Added test function and colors for the nucleotides can now be changed.

--- sequence_visualizer.py
+++ sequence_visualizer.py
@@ -3,12 +3,13 @@ Sequence visualization script to visualize a DNA sequence as a bitmap with a dif
 Author: Floris M
 Date: 14-04-2024
 Verion: 1
-
+commandline use: python sequence_visualizer.py Path_to_fasta_file Path_to_output_dir
 """
 
 
 from PIL import Image
 from itertools import zip_longest
+from datetime import date, datetime
 import sys
 # from pandas import factorize
 
@@ -24,28 +25,50 @@ def get_input():
 
        
 
-def open_fasta(sequence_path, output_dir):
+def open_fasta(sequence_path):
     """
     Opens the fasta file.
     """
     # seq_path = "/home/floris/OneDrive_FLORIS/Project_1/Bioinformatics/bitmap/testfasta.fasta"
     color_list = []
-    with open("/home/floris/OneDrive_FLORIS/Project_1/Bioinformatics/bitmap/testfasta.fasta", "r") as sequence:
+
+    # Colors for the "A" nucleotide:
+    a_color_red = 0
+    a_color_green = 0
+    a_color_blue = 255
+
+    # Colors for the "T" nucleotide:
+    t_color_red = 0
+    t_color_green = 255
+    t_color_blue = 255
+
+    # Colors for the "G" nucleotide:
+    g_color_red = 255
+    g_color_green = 0
+    g_color_blue = 0
+
+    # Colors for the "C" nucleotide:
+    c_color_red = 0
+    c_color_green = 255
+    c_color_blue = 0
+
+    with open(sequence_path, "r") as sequence:
         next(sequence) # skip header
         for line in sequence:
             for nucleotide in line.strip():
                 if nucleotide == "A":
-                    color_list.append(tuple((0,0,255))) # Blue color.
+                    color_list.append(tuple((a_color_red, a_color_green, a_color_blue))) # Blue color.
                 elif nucleotide == "T":
-                    color_list.append(tuple((255,255,255))) # white
+                    color_list.append(tuple((t_color_red, t_color_green, t_color_blue))) # white
                 elif nucleotide == "C":
-                    color_list.append(tuple((100,0,233))) # Purple
+                    color_list.append(tuple((c_color_red, c_color_green, c_color_blue))) # Purple
                 elif nucleotide == "G":
-                    color_list.append(tuple((255, 0, 0))) # Red
-                else: # Unrecognized nucleotide
-                    color_list.append(0) 
-                    print(f"Error, unrecognized nucleotide in sequence.... {nucleotide}")
-                    print("Placeholder value: 0 inserted.")
+                    color_list.append(tuple((g_color_red, g_color_green, g_color_blue))) # Red
+                # else: # Unrecognized nucleotide
+                    # color_list.append(0) 
+                    # print(f"Error, unrecognized nucleotide in sequence.... {nucleotide}")
+                    # print("Placeholder value: 0 inserted.")
+                    #return # If the program needs to stop when encountering a wrong nucleotide, uncomment this line. 
                    
             if ">" in line:
                     return color_list
@@ -55,32 +78,56 @@ def open_fasta(sequence_path, output_dir):
 
 
 
-def make_bmp(color_list):
-    img = Image.new( 'RGB', (1920,1080), "black") # create a new black image
+def make_bmp(color_list, ouput_dir):
+    img = Image.new( 'RGB', (16,8), "black") # create a new black image
     pixels = img.load() # create the pixel map
 
  # PIL accesses images in Cartesian co-ordinates, so it is Image[columns, rows]
     for i, color in zip(range(img.size[0]), color_list): # for every col:
-        print(f"color_l: {i} en {color}") 
+        #print(f"color_l: {i} en {color}") 
         for j in range(img.size[1]):    # For every row
-            pixels[i,j] = color[0], color[1], color[2] # set the colour accordingly
-           # pixels[i,j]=  234 # set the colour accordingly
+            print(f"Regel: {i} en kolom: {j}")
+            # pixels[i,j] = color[0], color[1], color[2] # set the colour accordingly
+            pixels[i,j] = i*9,j*9,100 # set the colour accordingly
+
 
     # for i in range(img.size[0]):    # for every col:
     #     for j in range(img.size[1]):    # For every row
 
-    #         pixels[i,j] = 44 # set the colour 
+    #         pixels[i,j] = (i, j, 100) # set the colour 
+
+def make_bmp_test(color_list, output_dir):
+    img = Image.new( 'RGB', (8,8), "black") # create a new black image
+    pixels = img.load() # create the pixel map
+
+ # PIL accesses images in Cartesian co-ordinates, so it is Image[columns, rows]
+    for  index, (i, color) in enumerate(zip(range(img.size[0]), color_list), start=0): # for every col:
+        pixels[index, index] = color # set the colour accordingly
+
+        for ii in range(img.size[0]):    # for every col:
+
+            print(f"color_l: {i} en {color}, index: {index}") 
+            pixels[index, ii] = color # set the colour accordingly
+
+
+    # for i in range(img.size[0]):    # for every col:
+    #     for j in range(img.size[1]):    # For every row
+
+    #         pixels[i,j] = (i, j, 100) # set the colour 
 
 
     img.show()
-    img.save("/home/floris/OneDrive_FLORIS/Project_1/Bioinformatics/bitmap/img.bmp", "BMP")
+    current_time = str(datetime.now())
+    img.save(output_dir + current_time + ".bmp", "BMP")
     print(len(color_list))
 
 
 if __name__ == "__main__":
     sequence_path, output_dir = get_input()
-    color_list = open_fasta(sequence_path, output_dir)
-    make_bmp(color_list)
+    color_list = open_fasta(sequence_path)
+    # make_bmp(color_list, output_dir)
+    make_bmp_test(color_list, output_dir)
+
 
 
 
